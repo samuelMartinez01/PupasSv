cd45666e5cd165b90dc9032885ffd80943355675
Samuel Martinez <mb21011@ues.edu.sv>
2025-06-20 09:45:12 -0600

    finish 2

--- src/test/java/sv/edu/ues/ingenieria/tpi135/pupassv/control/TipoProductoBeanIT.java
+++ src/test/java/sv/edu/ues/ingenieria/tpi135/pupassv/control/TipoProductoBeanIT.java
@@ -60,6 +60,7 @@ public class TipoProductoBeanIT {
     @Test
     @Order(2)
     public void testInsert() {
+         System.out.println("20 de junio");
         System.out.println("TipoProductoBeanIT.testInsert");
         TipoProductoBean cut = new TipoProductoBean();
         EntityManager em = emf.createEntityManager();
