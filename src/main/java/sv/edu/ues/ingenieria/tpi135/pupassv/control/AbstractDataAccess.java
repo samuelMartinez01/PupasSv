032bb48d9646918a4dd2a7eda0273069ee640f33
Samuel Martinez <mb21011@ues.edu.sv>
2025-06-19 23:00:38 -0600

    Iguana

--- src/main/java/sv/edu/ues/ingenieria/tpi135/pupassv/control/AbstractDataAccess.java
+++ src/main/java/sv/edu/ues/ingenieria/tpi135/pupassv/control/AbstractDataAccess.java
@@ -3,7 +3,7 @@
  * @param <T> Tipo de la entidad gestionada.
  */
 package sv.edu.ues.ingenieria.tpi135.pupassv.control;
-//Hola chepe
+//Hola chepe3
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.persistence.PersistenceException;
